db46503bb709782780ea82ca24c057e437051198
TinKrisKiss <z-qksi@yandex.ru>
2026-05-04 17:07:06 +0500

    333

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  site: 'https://tinkriskiss.github.io',
-  base: '/park-pobeda/',
   output: 'static',
-  trailingSlash: 'always',
-  compressHTML: true,
 });
